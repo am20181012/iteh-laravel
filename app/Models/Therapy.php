b8b258caa190669cc335cad3abe8465a3f02860e
am20181012 <andjelamijatovic98@gmail.com>
2022-05-09 23:49:46 +0200

    Kreirani modeli i tabele u bazi

---
+++ app/Models/Therapy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Therapy extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'diagnosis_id',
+        'type_of_therapy',
+        'name_of_therapy',
+        'description'
+    ];
+}
---
+++ database/migrations/2022_05_09_213642_create_patients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('card_num');
+            $table->string('name');
+            $table->timestamp('birthday');
+            $table->char('gender');
+            $table->string('adress');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+};
